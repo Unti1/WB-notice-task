c45b23c4861c38f88df6a5613741531986f3bb29
Unti1 <kostyuchenko.work@gmail.com>
2024-03-12 18:12:33 +0300

    editing time now saving

--- models/user.py
+++ models/user.py
@@ -6,7 +6,8 @@ class User(Base):
     
     # Определение столбцов таблицы
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer)  # User id in TG 
+    user_id = Column(Integer)  # User id in TG
+    time_add = Column(DateTime, default=datetime.now)
     article = Column(Integer)  # Item article
 
     def __init__(self, user_id, article):
--- tools/db.py
+++ tools/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from settings import *
